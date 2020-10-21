6dd0b737015802f1986d1afc4b3ba356ce5ba04a
Komiljon Kodirov <leaderandlucky@gmail.com>
2020-10-21 16:32:10 +0500

    .gitignore file was added

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func init() {
 }
 
 func main() {
-	bot = tbot.New(token)
+	bot = tbot.New(token, tbot.WithWebhook("https://tgbotgame.herokuapp.com", ":"+os.Getenv("PORT")))
 	app.client = bot.Client()
 	bot.HandleMessage("/start", app.startHandler)
 	bot.HandleMessage("/play", app.playHandler)
